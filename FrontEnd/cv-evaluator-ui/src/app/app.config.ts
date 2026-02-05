3e6420b53c3072a9e02fbad6f30e97b5de37af07
fraanrosi <fraanrosi@gmail.com>
2026-02-05 16:35:05 -0300

    removing apiKey

--- FrontEnd/cv-evaluator-ui/src/app/app.config.ts
+++ FrontEnd/cv-evaluator-ui/src/app/app.config.ts
@@ -1,16 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient} from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { apiKeyInterceptor } from './core/interceptors/api-key.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-
-    provideHttpClient(
-      withInterceptors([apiKeyInterceptor])
-    )
+    provideHttpClient()
   ]
 };
